dddbf8ed4c2b2f7aa33b6c7f31f28f8ac8085a26
Justin Cassidy <justincassidy@u.northwestern.edu>
2015-11-07 19:50:11 -0600

    This is the completed project 1
    Contains R code that generates the png files

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Plot 1 Code for Coursera Exploratory Data Analysis Course Project 1
+
+# Read in raw data as "plotdata" data frame
+read.table("household_power_consumption.txt",sep=";",header = TRUE)->plotdata
+
+# Convert Date and Time columns to a "datetime" column and convert to POSIXlt object
+plotdata$datetime = paste(as.character(plotdata$Date),as.character(plotdata$Time))
+plotdata$datetime=strptime(plotdata$datetime, "%d/%m/%Y %H:%M:%S")
+
+# Subset data frame to only the relevant days to graph for this exercise
+plotdataNEW <- subset(plotdata, datetime >= as.POSIXct('2007-02-01') & datetime < as.POSIXct('2007-02-03'))
+
+# Convert relevant field to numeric
+plotdataNEW$Global_active_power=as.numeric(as.character(plotdataNEW$Global_active_power))
+
+# Plots graph on screen device, then saves to appropriately labeled PNG file
+hist(plotdataNEW$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png,file="plot1.png",width=480,height=480)
+dev.off()
+
